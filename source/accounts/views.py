889cfd256a8cb36c6024f4317010c8a48a1d9c85
Oscar Garza <oscar35689@hotmail.com>
2017-10-17 21:42:36 -0500

    Added simple sign up and login, its creating a username and is able to log it in and checks if the username already exists and if its password is correct to test go to /login /signup and to /admin to check if user was created

---
+++ source/accounts/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+# Create your views here.
+def signUp(request):
+
+    #if the user sends infor its a post request
+    if request.method == 'POST':
+        if request.POST['password1'] == request.POST['password2']:
+             return createUserIfDoesNotExist(request)
+        else:
+            return render(request, 'accounts/signUp.html', {'error': 'Passwords didn\'t match'})
+    else:# its a get request
+        return render(request, 'accounts/signUp.html')
+
+def loginView(request):
+    #if the user sends infor its a post request
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request,user)
+            #redirect to succes page
+            return render(request, 'accounts/login.html', {'error': 'succesfull login'})
+        else:
+            #invalid user
+            return render(request, 'accounts/login.html', {'error': 'the username and password didn\'t match'})
+    else:# its a get request
+        return render(request, 'accounts/login.html')
+
+
+
+
+
+
+def createUserIfDoesNotExist(request):
+    try:
+        User.objects.get(username = request.POST['username'])
+        return render(request, 'accounts/signUp.html', {'error': 'Username has already been talem'})
+    except User.DoesNotExist:
+        user = User.objects.create_user(username =request.POST['username'],email = request.POST['email'], password = request.POST['password1'])
+        login(request, user)
+        return render(request, 'accounts/signUp.html')
