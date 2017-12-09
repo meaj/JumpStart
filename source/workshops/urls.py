73332148d98af22d857428479dc605c1dd70ec2d
meaj <kevin.omd@gmail.com>
2017-12-09 17:26:15 -0600

    Resolved Merge Conflicts

--- source/workshops/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-app_name = 'workshop'
-
-urlpatterns = [
-    url(r'^create/', views.createWorkshop, name='workshops'),
-    url(r'^(?P<pk>[0-9]+)/workshopdetails', views.workshop_details, name='workshopdetails'),
-]
--- source/workshops/views.py
+++ source/workshops/views.py
@@ -1,16 +1,15 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.utils import timezone
-from django.shortcuts import render, get_object_or_404
+from . import models
 
-from .models import Workshop
 
 # Create your views here.
 @login_required(login_url='/login/')
 def createWorkshop(request):
     if request.method == 'POST':
         if request.POST['title']:
-            workshop = Workshop()
+            workshop = models.Workshop()
             workshop.title = request.POST['title']
             workshop.pub_date = timezone.datetime.now()
             workshop.owner = request.user
@@ -21,8 +20,3 @@ def createWorkshop(request):
                           {'error': 'ERROR: You must include a Title'})
     else:
         return render(request, 'workshops/workshops.html')
-
-def workshop_details(request,pk):
-        workshop = get_object_or_404(Workshop, pk=pk)
-
-        return render(request,'workshops/workshopdetails.html',{'workshop':workshop})
